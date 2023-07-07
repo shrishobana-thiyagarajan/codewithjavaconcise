21ad36284fc12c50d72e1b90c1fd6b77a71ea295
shrishobana-thiyagarajan <rshrishobana@gmail.com>
2023-07-07 20:41:47 +0200

    operator program

---
+++ operatorExamples.java
@@ -0,0 +1,15 @@
+public class operatorExamples {
+    public static void main(String[] args){
+        // expression = operands & operator
+        // operands =values,variables,numbers,quantity
+        //operators = +-*/%
+
+        double number =10;
+        number++;
+        System.out.println("number"+number);
+        number--;
+        System.out.println("number"+number);
+        number=(double)number/3;
+        System.out.println("number"+number);
+    }
+}
