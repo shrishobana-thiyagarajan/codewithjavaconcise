313666f4f04e6ff3e23c6959876ed5e9927a2a9f
shrishobana-thiyagarajan <rshrishobana@gmail.com>
2023-07-09 19:01:07 +0200

    Variablecode

---
+++ PracticeSessions/variablesexample/variablesexample.java
@@ -0,0 +1,16 @@
+public class variablesexample {
+    public static void main(String[] args)
+    {
+        int x=123;
+        double y= 3.14;
+        boolean z =true;
+        char symbol ='@';
+        String name ="Variables";
+        System.out.println("My number is Variables int"+x);
+        System.out.println("My number is Variables double"+y);
+        System.out.println("My number is Variables char"+symbol);
+        System.out.println("My number is Variables boolean"+z);
+        System.out.println("My number is Variables String"+name);
+
+    }
+}
